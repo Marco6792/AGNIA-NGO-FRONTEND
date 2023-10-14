848bf2c23aa52f77d4f7bfd7937dd8368cb8bff5
Marco6792 <110029449+Marco6792@users.noreply.github.com>
2023-10-14 08:34:52 +0100

    fghgfhf

--- src/components/Home/HeroPage.tsx
+++ src/components/Home/HeroPage.tsx
@@ -10,7 +10,7 @@ const HeroPage = () => {
 			<p className="max-sm:text-slate-100 shadow-md p-6 rounded-md max-sm:from-slate-500 max-sm:to-slate-900 bg-gradient-to-tr bg-opacity-10 text-base max-w-xl md:ml-[3rem] ">
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, deserunt nihil.
 				Similique reprehenderit laboriosam asperiores rerum molestiae ducimus ipsa perferendis
-				praesentium. Facilis amet labore deleniti mollitia porro modi sed similique!
+				praesentium. Facilis amet labore deleniti mollitia porro modi 
 			</p>
       </div>
       <div className="from-blue-300 to-blue-400 bg-gradient-to-tr p-6 fixed -top-10  max-md:-right-40 opacity-10 h-[300px] w-[470px] rounded-full -rotate-45 md:hidden">
