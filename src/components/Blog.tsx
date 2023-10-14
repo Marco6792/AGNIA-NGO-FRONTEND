aa8f25c19cc1b5ffb13f70c8ec57139018ddc8bc
Marco6792 <110029449+Marco6792@users.noreply.github.com>
2023-10-14 15:19:25 +0100

    asset commit

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import VIDEO1 from "../assets/VID-20231008-WA0001.mp4"
+import VIDEO2 from "../assets/VID-20231012-WA0002.mp4"
 const Blog = () => {
 	return (
 		<div className="flex flex-col justify-center items-center pt-10  md:max-w-4xl mx-auto relative">
@@ -8,13 +9,13 @@ const Blog = () => {
 			</h1>
 			<div className=" mt-10 w-full max-sm:space-y-5 px-4 pb-6  grid grid-cols-2 gap-10 pt-10 max-sm:grid-cols-1 text-white max-sm:px-1">
 				<div className="border max-sm:w-full rounded-md z-20 from-slate-300 to-slate-800 bg-gradient-to-r">
-					<video src={VIDEO1} typeof="mp4" controls className="w-full h-[100%]" title="introduction"></video>
+					<video src={VIDEO2} typeof="mp4" controls className="w-full h-[100%]" title="introduction"></video>
 				</div>
 				<div className="border p-10 max-sm:w-full rounded-md z-20 from-slate-300 to-slate-800 bg-gradient-to-r">
 					<h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem soluta delectus, tempore dolorem dolores quibusdam eos ab fugit itaque deserunt eveniet nostrum? Id, voluptate hic. Aliquam, accusantium non? Cum, numquam?</h1>
 				</div>
 				<div className="border p-10 max-sm:w-full rounded-md z-20 from-slate-300 to-slate-800 bg-gradient-to-r">
-					<h1>Post three (3)</h1>
+				<video src={VIDEO1} typeof="mp4" controls className="w-full h-[100%]" title="introduction"></video>
 				</div>
 				<div className="border p-10 max-sm:w-full rounded-md z-20 from-slate-300 to-slate-800 bg-gradient-to-r">
 					<h1>Post four</h1>
