99bc8b93b44851b85434151f90cbdd6061436bd4
Marco6792 <110029449+Marco6792@users.noreply.github.com>
2023-10-14 15:28:04 +0100

    reeeeeh

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -18,7 +18,7 @@ const Blog = () => {
 				<video src={VIDEO1} typeof="mp4" controls className="w-full h-[100%]" title="introduction"></video>
 				</div>
 				<div className="border p-10 max-sm:w-full rounded-md z-20 from-slate-300 to-slate-800 bg-gradient-to-r">
-					<h1>Post four</h1>
+					<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci explicabo blanditiis nemo culpa modi ipsum id at pariatur sit tenetur, incidunt quis eaque dolor obcaecati. Officia similique illum ipsam id?</h1>
 				</div>
 			</div>
 			<div className="from-orange-50 to-purple-200 bg-gradient-to-bl p-6 fixed  md:right-80 opacity-10 h-[400px] w-[400px] rounded-full left-1/2 right-1/2 top-1/2 max-sm:left-0"></div>
